9bc00d4ecca0fa4cd52d4ad1c29cc4641b0688f5
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-04-13 21:49:34 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -111,11 +111,9 @@ def ping(host, timeout=1):
    stddev = 0
    for i in lst:
        stddev += (i - packet_avg)**2
-    stddev.math.sqrt((stddev/len(lst)))
+       stddev.math.sqrt((stddev/len(lst)))
 
-
-
-   vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),
+    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),
            str(round(stdev(stdev_var), 8))]
 
    return vars
