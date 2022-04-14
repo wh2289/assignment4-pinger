56d82969a422bafd9d524eedf5b2fa69d940ce55
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-04-13 22:16:42 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -106,7 +106,7 @@ def ping(host, timeout=1):
    for i in range(0,4):
        delay = doOnePing(dest, timeout)
        print(delay)
-       lst.append(round(delay[0] * 1000, 8))
+
        time.sleep(1)
 
    packet_min = min(lst)
@@ -116,6 +116,7 @@ def ping(host, timeout=1):
    for i in lst:
        stdev += (i - packet_avg)**2
        stdev.math.sqrt((stddev/len(lst)))
+       lst.append(round(delay[0] * 1000, 8))
        #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),
             #str(round(stdev(stdev_var), 8))]
    return vars
