0b7d3dcb5d8446626e69933a094851f05807c8b6
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-04-15 21:53:03 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -100,8 +100,7 @@ def ping(host, timeout=1):
     stdev_var = 0
     delay = 0
 
-    lst = []
-
+    lst = [delay]
 
     for i in range(0, 4):
         delay = doOnePing(dest, timeout)
@@ -109,15 +108,15 @@ def ping(host, timeout=1):
         lst.append(delay[0])
         time.sleep(1)
 
-    packet_min = min(lst)
-    packet_max = max(lst)
-    packet_avg = sum(lst) / len(lst)
-    stdev_var = lst
+packet_min = min(lst)
+packet_max = max(lst)
+packet_avg = sum(lst) / len(lst)
+stdev_var = lst
 
-    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),
+vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),
             str(round(stdev(stdev_var), 8))]
 
-    return vars
+return vars
 
 
 if __name__ == '_main_':
